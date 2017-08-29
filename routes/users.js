40f19e09dc30936c80e0124df6d91cde6eb07e71
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-08-29 20:29:51 +0200

    add mongoose, bcrypt and so on

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 
+var User = require('../models/user');
+const userViewPath = '../views/user';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	User.find({}).then((all_users) => {
+		console.log('all user route', all_users);
+		res.render(`${userViewPath}/index`, {all_users, title: "All Users"});
+	})
 });
+
+router.post('/add_sample', function(req,res,next) {
+	var sampleUser = new User({
+		username: 'sampleUser',
+		password: 'samplePassword'
+	});
+
+	sampleUser.save().then((user) => {
+		console.log('user added:', user);
+	}).catch((err) => {
+		console.error(err);
+		res.send('wrong');
+	})
+
+});
+
+router.get('/login', (req,res,next) => {
+	res.render(`${userViewPath}/login`, {username:''});
+})
+
+router.post('/login', (req,res,next) => {
+	var {username, password} = req.body;
+	var hash;
+	var findUser = User.findOne({username: username});
+	findUser.then((user) => {
+		hash = user.password;
+			bcrypt.compare(password, hash).then(function(valid) {
+    // valid == true
+		    if (valid) {
+		    	console.log('matching passwords');
+					res.redirect('/');
+
+		    } else {
+		    	res.render(`${userViewPath}/login`, {username});
+		    }
+
+			});
+	}).catch((err) => {
+		console.log('no user found');
+	})
+})
 
 module.exports = router;
--- server/app.js
+++ server/app.js
@@ -1,24 +1,30 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+const http = require('http');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('../routes/index');
+var users = require('../routes/users');
+
+require('./mongoose');
 
 var app = express();
+// var server = http.createServer(app);
+
+const PORT = process.env.PORT || 8080;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'..', 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,11 +42,21 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development'
+    ? err
+    : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+// server.listen(PORT, () => {
+//   console.log(`Server running at ${PORT}`);
+// })
+
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+})
 
 module.exports = app;
